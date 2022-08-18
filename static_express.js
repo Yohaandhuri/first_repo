1820413d72c126ad5d96070709976aa214928c0e
yohaan_dhuri <yohaandhuri@gmail.com>
2022-08-18 19:27:39 +0530

    express commited on express branch

---
+++ static_express.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+ 
+
+app.use("path of static folder");
+
+app.get('/',(req,res)=>{
+    app.sendFile(path.resolve(__dirname,"path"));
+})
+
+app.all('*',(req,res)=>{
+    res.status(404).send('error');
+})
+
+app.listen(5000,()=>{
+    console.log('user is listening on 5000');
+})
